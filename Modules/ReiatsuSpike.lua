d4d96099db3feb73e3538596e62bc10b7b842df4
John Doe <johndoe@example.com>
2025-08-07 02:25:03 -0700

    Refine dynamic timing calculations for ReiatsuSpike, ReishiWave, and TriCero modules

--- Modules/ReiatsuSpike.lua
+++ Modules/ReiatsuSpike.lua
@@ -8,7 +8,7 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = math.min(440 + distance * 9, 1600)
+	action._when = math.min(400 + distance * 7, 1600)
 	action._type = "Parry"
 	action.hitbox = Vector3.new(15, 15, 100)
 	action.name = string.format("(%.2f) Dynamic Reiatsu Spike Timing", distance)
--- Modules/TriCero.lua
+++ Modules/TriCero.lua
@@ -6,10 +6,16 @@ local Action = getfenv().Action
 ---@param timing AnimationTiming
 return function(self, timing)
 	task.wait(0.1)
+	local speed = self.track.Speed
 	local action = Action.new()
-	action._when = (700 * 2.00) / self.track.Speed
+	action._when = 700
 	action._type = "Parry"
 	action.hitbox = Vector3.new(30, 20, 150)
-	action.name = string.format("(%.2f) Dynamic Tri Cero Timing", self.track.Speed)
+	action.name = string.format("(%.2f) Dynamic Tri Cero Timing", speed)
+
+	if speed > 1.0 then
+		action._when = 310
+	end
+
 	return self:action(timing, action)
 end
