44040301deca52bd2d551fe1806614fbf442d65b
John Doe <johndoe@example.com>
2025-08-15 02:35:47 -0700

    ccccc

--- Modules/Carve.lua
+++ Modules/Carve.lua
@@ -8,13 +8,13 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = 400
-	if distance >= 11 then
-		action._when = math.min(290 + distance * 22.5, 2000)
+	action._when = 360
+	if distance >= 16 then
+		action._when = math.min(150 + distance * 18, 1000)
 	end
 	action._type = "Parry"
-	action.hitbox = Vector3.new(25, 15, 30)
-	action.name = string.format("(%.2f) Dynamic Water X Timing", distance)
+	action.hitbox = Vector3.new(22, 15, 35)
+	action.name = string.format("(%.2f) Dynamic Carving Timing", distance)
 
 	return self:action(timing, action)
 end
--- Modules/Justice.lua
+++ Modules/Justice.lua
@@ -7,18 +7,18 @@ local Action = getfenv().Action
 return function(self, timing)
 	local distance = self:distance(self.entity)
 	local action = Action.new()
-	action._when = 420
-	if distance >= 18 then
-		action._when = 440
-	end
+	action._when = 400
 	if distance >= 21 then
 		action._when = 660
 	end
 	if distance >= 42 then
 		action._when = 900
 	end
+	if distance >= 64 then
+		action._when = 1180
+	end
 	action._type = "Parry"
-	action.hitbox = Vector3.new(30, 20, 70)
+	action.hitbox = Vector3.new(30, 20, 100)
 	action.name = string.format("(%.2f) Dynamic Justice Timing", distance)
 	return self:action(timing, action)
 end
