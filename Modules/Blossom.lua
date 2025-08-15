186ae691aa1def69929745617f6ea317f90384e2
John Doe <johndoe@example.com>
2025-08-14 21:20:34 -0700

    casdawda

--- Modules/Blossom.lua
+++ Modules/Blossom.lua
@@ -8,7 +8,7 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = math.min(150 + distance * 7, 500)
+	action._when = math.min(150 + distance * 6.5, 500)
 	action._type = "Parry"
 	action.hitbox = Vector3.new(60, 15, 70)
 	action.name = string.format("(%.2f) Dynamic Blossom Timing", distance)
