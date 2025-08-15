ded9c16e38d3f918ef1e30168dc2cc1dc6d90ea8
John Doe <johndoe@example.com>
2025-08-14 17:57:28 -0700

    yooooo

--- Modules/Confliction.lua
+++ Modules/Confliction.lua
@@ -6,12 +6,13 @@ local Action = getfenv().Action
 ---@param timing AnimationTiming
 return function(self, timing)
 	local distance = self:distance(self.entity)
-
 	local action = Action.new()
-	action._when = math.min(600 + distance * 13, 1800)
+	action._when = 400
+	if distance >= 9 then
+		action._when = math.min(540 + distance * 9, 1000)
+	end
 	action._type = "Parry"
-	action.hitbox = Vector3.new(55, 55, 55)
-	action.name = string.format("(%.2f) Dynamic Fear Timing", distance)
-
+	action.hitbox = Vector3.new(14, 15, 17)
+	action.name = string.format("(%.2f) Dynamic Confliction Timing", distance)
 	return self:action(timing, action)
 end
