43ad8bb9fec124ee55ce6d88f5d2f9f604a5776d
Blastbrean <blastbrean@waifu.club>
2025-08-17 16:33:07 -0700

    Refactor code structure for improved readability and maintainability

--- Modules/MedicalRod.lua
+++ Modules/MedicalRod.lua
@@ -8,9 +8,21 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = math.min(770 + distance * 7, 1500)
+
+	-- pattern seems to be for every distance increment of 12 += 50
+	-- 3 : 650
+	-- 9-11 : 650
+	-- 11-23 : 700
+	-- 23-34: 750
+
+	action._when = 650
+
+	if distance >= 12 then
+		action._when += math.floor(distance / 12) * 50
+	end
+
 	action._type = "Parry"
-	action.hitbox = Vector3.new(100, 30, 100)
+	action.hitbox = Vector3.new(100, 30, 300)
 	action.name = string.format("(%.2f) Dynamic Medical Rod Timing", distance)
 
 	return self:action(timing, action)
