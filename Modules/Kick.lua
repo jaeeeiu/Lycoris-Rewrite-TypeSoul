3da12178bc6ae206fed04e7efddf8d0f3f9462c1
John Doe <johndoe@example.com>
2025-08-17 18:52:10 -0700

    codes and tings

--- Modules/Kick.lua
+++ Modules/Kick.lua
@@ -8,9 +8,9 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = math.min(1250 + distance * 1.8, 1700)
+	action._when = math.min(1250 + distance * 5, 2500)
 	action._type = "Parry"
-	action.hitbox = Vector3.new(100, 120, 100)
+	action.hitbox = Vector3.new(150, 120, 150)
 	action.name = string.format("(%.2f) Dynamic Bomb Kick Timing", distance)
 
 	return self:action(timing, action)
