632a8170b3c5464d659a03501470a4feb235b244
John Doe <johndoe@example.com>
2025-08-20 01:07:23 -0700

    changes

--- Modules/FingerCero.lua
+++ Modules/FingerCero.lua
@@ -7,12 +7,12 @@ local Action = getfenv().Action
 return function(self, timing)
 	repeat
 		task.wait()
-	until self.track.TimePosition >= 1.355
+	until self.track.TimePosition >= 0.375
 
 	local action = Action.new()
 	action._when = 0
 	action._type = "Parry"
-	action.hitbox = Vector3.new(35, 25, 120)
-	action.name = string.format("(%.2f) Dynamic Cero Oscuras Timing", self.track.Speed)
+	action.hitbox = Vector3.new(15, 15, 120)
+	action.name = string.format("(%.2f) Dynamic Finger Cero Timing", self.track.Speed)
 	self:action(timing, action)
 end
