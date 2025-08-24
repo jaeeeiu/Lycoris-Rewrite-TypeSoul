dde0d2446ba68404680c0f8671adc4f15637c468
Blastbrean <blastbrean@waifu.club>
2025-08-23 17:56:17 -0700

    fix: improve error handling and formatting for 'smod' in Zeke Hub processing

--- Funny.py
+++ Funny.py
@@ -101,7 +101,6 @@ for key, timing in anim_table_3.items():
     try:
         int(key)
     except ValueError:
-        print(f"(Zeke Hub) Skipping key {key} because it is not an animation")
         continue
 
     timing = find_timing_from_timing_id("rbxassetid://" + key)
@@ -109,10 +108,9 @@ for key, timing in anim_table_3.items():
     if not timing:
         continue
 
-    if timing.smod.endswith(".lua"):
-        timing.smod = timing.smod[:-4]
-
-    print(f"(Zeke Hub) Fixed 'smod' for rbxassetid://{key}")
+    if timing.get("smod").endswith(".lua"):
+        timing["smod"] = timing["smod"][:-4]
+        print(f"(Zeke Hub) Fixed 'smod' for rbxassetid://{key}")
 
 for key, timing in anim_table_3.items():
     try:
