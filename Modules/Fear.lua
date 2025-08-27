224533453656e396e3f2a04e75101f9f4e0ea8d9
John Doe <johndoe@example.com>
2025-08-26 17:45:25 -0700

    uhhhhh volt additions

--- Modules/Fear.lua
+++ Modules/Fear.lua
@@ -8,10 +8,21 @@ return function(self, timing)
 	local distance = self:distance(self.entity)
 
 	local action = Action.new()
-	action._when = math.min(630 + distance * 13, 1800)
-	action._type = "Parry"
-	action.hitbox = Vector3.new(55, 55, 55)
+	action._when = 680
+	if distance >= 9 then
+		action._when = math.min(750 + distance * 19, 1000)
+	end
+	if distance >= 21 then
+		action._when = math.min(990 + distance * 2, 1800)
+	end
+	action._type = "Start Block"
+	action.hitbox = Vector3.new(81, 70, 81)
 	action.name = string.format("(%.2f) Dynamic Fear Timing", distance)
+	self:action(timing, action)
 
-	return self:action(timing, action)
+	local action2 = Action.new()
+	action2._when = 2000
+	action2._type = "End Block"
+	action2.hitbox = Vector3.new(120, 120, 120)
+	self:action(timing, action2)
 end
--- Modules/NervePull.lua
+++ Modules/NervePull.lua
@@ -7,16 +7,12 @@ local Action = getfenv().Action
 return function(self, timing)
 	local distance = self:distance(self.entity)
 	local action = Action.new()
-	action._when = math.min(385 + distance * 15, 1700)
-	action._type = "Start Block"
+	action._when = math.min(420 + distance * 4, 750)
+	if distance >= 26 then
+		action._when = math.min(285 + distance * 18, 1700)
+	end
+	action._type = "Parry"
 	action.hitbox = Vector3.new(55, 20, 70)
 	action.name = string.format("(%.2f) Dynamic Nerve Pull Timing", distance)
-	self:action(timing, action)
-
-	local action2 = Action.new()
-	action2._when = 1500
-	action2._type = "End Block"
-	action2.hitbox = Vector3.new(120, 120, 120)
-	action2.name = string.format("(2) (%.2f) Dynamic Timing", distance)
-	self:action(timing, action2)
+	return self:action(timing, action)
 end
