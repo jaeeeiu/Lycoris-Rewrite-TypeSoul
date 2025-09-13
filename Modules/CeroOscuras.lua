44de781e0ddb9db5642700bf158f639bf5434f14
John Doe <johndoe@example.com>
2025-09-12 21:05:41 -0700

    changes

--- Modules/CeroOscuras.lua
+++ Modules/CeroOscuras.lua
@@ -5,9 +5,14 @@ local Action = getfenv().Action
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
+	repeat
+		task.wait()
+	until self.track.TimePosition >= 1.41
+
 	local action = Action.new()
+	action._when = 0
 	action._type = "Parry"
-	action.hitbox = Vector3.new(35, 25, 170)
-	action.name = "Keyframe Action"
-	return self:akeyframe(action, 1.34)
+	action.hitbox = Vector3.new(15, 15, 120)
+	action.name = string.format("(%.2f) Dynamic Cero Oscuras Timing", self.track.Speed)
+	self:action(timing, action)
 end
